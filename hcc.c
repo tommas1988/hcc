c839dc64fc1c846c43e8f3f4f9990e3ab986d32d
Tommas <tommas@localhost.localdomain>
2017-08-05 10:06:58 +0800

    loop files/dirs

--- hcc.c
+++ hcc.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <getopt.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
 
-static void usageInfo(const char *msg) {
-  if (msg) {
-    puts(msg);
-  }
-
+static void usageInfo() {
   puts("Usage: hcc [OPTION]... [FILE]...");
   puts("Count the actual code lines in each file");
+}
+
+static void scanFile() {
+  puts("scan a code file");
+}
+
+static void scanDir() {
+  puts("scan from directory");
 }
 
 int main(int argc, char *argv[]) {
@@ -16,14 +23,15 @@ int main(int argc, char *argv[]) {
     { "help", no_argument, NULL, 'h'},
     { NULL, 0, NULL, 0},
   };
-  int opt;
+  int opt, i;
   char *filename;
+  struct stat sb;
 
   while ((opt = getopt_long(argc, argv, shortOpts, longOpts, NULL)) != -1) {
     switch (opt) {
     case 'h':
     case '?':
-      usageInfo(NULL);
+      usageInfo();
       break;
     default:
       /* never reach to this place */
@@ -32,8 +40,28 @@ int main(int argc, char *argv[]) {
   }
 
   if (!argv[optind]) {
-    usageInfo("File or directory argument is required");
+    fputs(stderr, "File or directory argument is required");
+    usageInfo();
+    exit(EXIT_FAILURE);
   }
 
-  return 0;
+  for (i = optind; argv[i]; i++) {
+    /* reset stat buffer */
+    memset(&sb, 0, sizeof(struct stat));
+    stat(argv[i], &sb);
+
+    switch (sb.st_mode & S_IFMT) {
+    case S_IFREG:
+      scanFile();
+      break;
+    case S_IFDIR:
+      scanDir();
+      break;
+    default:
+      fprintf(stderr, "Error: cannot locat file or directory: %s\n", argv[i]);
+      break;
+    }
+  }
+
+  exit(EXIT_SUCCESS);
 }
